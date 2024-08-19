51144f2aaaca2981b096b61012ba709799668293
Ela1009 <ela.vrcelj@gmail.com>
2024-08-19 16:04:48 +0200

    First exercise
    This program prompts the user to enter their name and then displays a personalized greeting. It demonstrates the use of the Scanner class for user input and basic string concatenation in Java.
    
    Concepts Covered:
    
    Using the Scanner class from java.util for input handling.
    Reading a line of text with nextLine() method.
    Basic string operations, including concatenation.
    Simple input/output operations.

---
+++ First.java
@@ -0,0 +1,14 @@
+import java.lang.*;
+import java.util.*;
+
+class First
+{
+	public static void main(String args[])
+		{
+			String name;
+			Scanner sc=new Scanner(System.in);
+			System.out.println("Enter your name please");
+			name=sc.nextLine();
+			System.out.println("Welcome "+name);
+		}
+}
