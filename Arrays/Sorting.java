c56ff113e215a7c369eb4eb1b18374d909542abb
Ela1009 <ela.vrcelj@gmail.com>
2024-08-30 21:01:08 +0200

    String Array Sorting
    This program sorts an array of strings in alphabetical order using Java's built-in sorting functionality.
    
    Concepts Covered:
    
    Sorting arrays in Java.
    Use of the Arrays.sort() method.
    Lexicographical order for string sorting.
    Enhanced for loop for iterating through arrays.

---
+++ Arrays/Sorting.java
@@ -0,0 +1,15 @@
+package sorting;
+
+public class Sorting {
+
+    public static void main(String[] args) {
+        String arr[]={"java","ada","basic","cs"};
+        
+        java.util.Arrays.sort(arr);
+        
+        for (String x:arr)
+        {
+            System.out.println(x);
+        }
+    }  
+}
