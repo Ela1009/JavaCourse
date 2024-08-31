d962bb9672c4e20f9402c7a094ad64afff84dca2
Ela1009 <ela.vrcelj@gmail.com>
2024-08-31 09:38:50 +0200

    Method Overloading
    This program demonstrates the concept of method overloading in Java. The program defines two overloaded methods named area to calculate the area of different geometric shapes: a circle and a rectangle.
    
    Concepts Covered:
    
    Method overloading in Java.
    Calculating the area of different geometric shapes using overloaded methods.
    Understanding how Java differentiates between overloaded methods based on their parameters.

---
+++ Methods/Overloading.java
@@ -0,0 +1,20 @@
+package overloading;
+
+public class Overloading {
+    
+    static double area(double radius)
+    {
+        return Math.PI*radius*radius;
+    }
+    
+    static double area(double length,double breadth)
+    {
+        return length*breadth;
+    }
+    
+    
+    public static void main(String[] args) {
+        System.out.println(area(5,6));
+    }
+    
+}
